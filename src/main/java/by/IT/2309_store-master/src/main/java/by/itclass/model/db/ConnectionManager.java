b4ee76ee2cf6330a465cddc5ced5a9e67a049cd0
Asus <vaxa2320@gmail.com>
2025-01-12 18:08:29 +0300

    01

--- src/main/java/by/IT/2309_store-master/src/main/java/by/itclass/model/db/ConnectionManager.java
+++ src/main/java/by/IT/2309_store-master/src/main/java/by/itclass/model/db/ConnectionManager.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConnectionManager {
+
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/2309_store";
     private static final String USER = "root";
